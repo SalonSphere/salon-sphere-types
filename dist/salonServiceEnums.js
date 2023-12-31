b48181d19f1cfd7ef4dbaeab68483b112a2daafc
edapess <edmondavetisyanw@gmail.com>
2023-12-31 15:19:19 +0400

    test: typescript

---
+++ dist/salonServiceEnums.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.BeautySalonServices = exports.BarbershopServices = void 0;
+var BarbershopServices;
+(function (BarbershopServices) {
+    BarbershopServices["HAIRCUT"] = "Haircut";
+    BarbershopServices["SHAVE"] = "Shave";
+    BarbershopServices["BEARD_TRIM"] = "Beard Trim";
+    BarbershopServices["HAIR_STYLING"] = "Hair Styling";
+})(BarbershopServices || (exports.BarbershopServices = BarbershopServices = {}));
+var BeautySalonServices;
+(function (BeautySalonServices) {
+    BeautySalonServices["MANICURE"] = "Manicure";
+    BeautySalonServices["PEDICURE"] = "Pedicure";
+    BeautySalonServices["FACIAL"] = "Facial";
+    BeautySalonServices["WAXING"] = "Waxing";
+})(BeautySalonServices || (exports.BeautySalonServices = BeautySalonServices = {}));
---
+++ src/salonServiceEnums.ts
@@ -0,0 +1,13 @@
+export enum BarbershopServices {
+  HAIRCUT = "Haircut",
+  SHAVE = "Shave",
+  BEARD_TRIM = "Beard Trim",
+  HAIR_STYLING = "Hair Styling",
+}
+
+export enum BeautySalonServices {
+  MANICURE = "Manicure",
+  PEDICURE = "Pedicure",
+  FACIAL = "Facial",
+  WAXING = "Waxing",
+}
