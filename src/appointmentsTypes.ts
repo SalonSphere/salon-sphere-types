bdeabaccd2d44d7b480015deabb2cabecb22d958
edapess <edmondavetisyanw@gmail.com>
2024-01-02 01:37:36 +0400

    feat: salon error message

--- src/appointmentsTypes.ts
+++ src/appointmentsTypes.ts
@@ -6,6 +6,7 @@ export enum EAppointmntError {
 export enum EAppointmntMessages {
   SUCCESS = "Appointment created",
   NO_USER_WITH_ID = "No user with id",
+  NO_SALON_WITH_ID = "No user with id",
   NO_APPOINTMENT_WITH_ID = "No appointment found with id",
   APPOINTMENT_DELETED = "Appointment successfully deleted",
   APPOINTMENT_UPDATED = "Appointment successfully updated",
