5e964f39c94908d38b876d5576b4bec6c37493e9
edapess <edmondavetisyanw@gmail.com>
2024-01-02 01:41:51 +0400

    fix: typo

--- dist/appointmentsTypes.d.ts
+++ dist/appointmentsTypes.d.ts
@@ -6,6 +6,7 @@ export declare enum EAppointmntError {
 export declare enum EAppointmntMessages {
     SUCCESS = "Appointment created",
     NO_USER_WITH_ID = "No user with id",
+    NO_SALON_WITH_ID = "No salon with id",
     NO_APPOINTMENT_WITH_ID = "No appointment found with id",
     APPOINTMENT_DELETED = "Appointment successfully deleted",
     APPOINTMENT_UPDATED = "Appointment successfully updated"
--- dist/appointmentsTypes.js
+++ dist/appointmentsTypes.js
@@ -11,6 +11,7 @@ var EAppointmntMessages;
 (function (EAppointmntMessages) {
     EAppointmntMessages["SUCCESS"] = "Appointment created";
     EAppointmntMessages["NO_USER_WITH_ID"] = "No user with id";
+    EAppointmntMessages["NO_SALON_WITH_ID"] = "No salon with id";
     EAppointmntMessages["NO_APPOINTMENT_WITH_ID"] = "No appointment found with id";
     EAppointmntMessages["APPOINTMENT_DELETED"] = "Appointment successfully deleted";
     EAppointmntMessages["APPOINTMENT_UPDATED"] = "Appointment successfully updated";
