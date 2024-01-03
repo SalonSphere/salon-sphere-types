202f2c3fcdded84d8ca0c13d6444ace7f85cce17
edapess <edmondavetisyanw@gmail.com>
2024-01-03 23:01:57 +0400

    fix:  export types

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ export * from "./salonServiceTypes";
 export * from "./appointmentsTypes";
 export * from "./authTypes";
 export * from "./dataRelatedTypes";
+export * from "./salonTypes";
+export * from "./userTypes";
